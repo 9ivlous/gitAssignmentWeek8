9b65364f34340ff768365026f5312f13734d6799
Nivlous <acfallarme@paterostechnologicalcollege.edu.ph>
2024-04-19 17:14:52 +0800

    my first commit

--- Prog1.java
+++ Prog1.java
@@ -1,7 +1,6 @@
 public class Pro1 {
    public static void main(String[] args){
    System.out.println("PATEROS");
-   System.out.println("TECHNOLOGICAL");
-   System.out.println("COLLEGE");
+  
 }
 }
