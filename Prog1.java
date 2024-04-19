0074209e186c6bc5ce7bfb79d79913d99af74f2f
Nivlous <acfallarme@paterostechnologicalcollege.edu.ph>
2024-04-19 17:18:37 +0800

    added three lines of code

--- Prog1.java
+++ Prog1.java
@@ -1,1 +1,7 @@
-java file
+public class Pro1 {
+   public static void main(String[] args){
+   System.out.println("PATEROS");
+   System.out.println("TECHNOLOGICAL");
+   System.out.println("COLLEGE");
+}
+}
